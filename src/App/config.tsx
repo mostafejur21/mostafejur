85392378691b6b8c786ff1e754b1f4c162598b3d
Sudipta Das <shiningsudipto@gmail.com>
2024-09-23 17:07:36 +0600

    first commit

---
+++ src/App/config.tsx
@@ -0,0 +1,41 @@
+import { Config } from 'types';
+import { Email, GitHub, LinkedIn, Resume } from 'icons';
+
+export const config: Config = {
+  name: {
+    display: 'Mostafejur Rahman',
+  },
+  title: {
+    display: 'Software Engineer',
+  },
+  buttons: [
+    {
+      name: 'github',
+      display: 'GitHub',
+      ariaLabel: 'GitHub profile (opens in new window)',
+      icon: <GitHub />,
+      href: 'https://github.com/mostafejur21/',
+    },
+    {
+      name: 'linked-in',
+      display: 'LinkedIn',
+      ariaLabel: 'LinkedIn profile (opens in new window)',
+      icon: <LinkedIn />,
+      href: 'https://www.linkedin.com/in/mostafejur21/',
+    },
+    {
+      name: 'resume',
+      display: 'Resume',
+      ariaLabel: 'Resume in Google Drive (opens in new window)',
+      icon: <Resume />,
+      href: 'https://drive.google.com/file/d/1wwagHyPEvC5eS-84biit1qWDvRZBycX0/view?usp=sharing',
+    },
+    {
+      name: 'email',
+      display: 'Email',
+      ariaLabel: 'Email contact (opens in new window)',
+      icon: <Email />,
+      href: 'mailto:info.mostafejur21@gmail.com',
+    },
+  ],
+};
